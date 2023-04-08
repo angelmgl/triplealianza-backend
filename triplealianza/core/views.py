80a2931897e17b0c6e8231b87a029c26ff7ca12b
Angel Alemany <angelemegeele@gmail.com>
2023-04-07 23:50:03 -0400

    Test gunicorn

--- triplealianza/core/views.py
+++ triplealianza/core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def welcome(request):
-    response = {'title': 'Welcome!'}
+    response = {'title': 'Welcome to triplealianza.com.py!'}
     return JsonResponse(response)
