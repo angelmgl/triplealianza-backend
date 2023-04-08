2a3e28dd94960c1c6e86fe828189c53be472618a
Angel Alemany <angelemegeele@gmail.com>
2023-04-08 16:57:06 -0400

    Add CORS settings

--- triplealianza/core/views.py
+++ triplealianza/core/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def welcome(request):
-    response = {'title': 'Welcome to triplealianza.com.py!'}
+    response = {
+        'title': 'Welcome to triplealianza.com.py!',
+        'description': 'Hey! We are under construction right now, but don\'t worry! We are working hard to get everything ready soon and provide you with the best content! Thanks for your patience and see you soon! 🚧👷‍♂️👷‍♀️'
+    }
     return JsonResponse(response)
--- triplealianza/triplealianza/settings.py
+++ triplealianza/triplealianza/settings.py
@@ -27,11 +27,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -112,3 +114,12 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_ROOT = BASE_DIR / "static"
+
+# CORS settings
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "https://triplealianza.com.py",
+] 
+CORS_ALLOW_CREDENTIALS = True
