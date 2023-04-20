9d34511f8eed817f641cfd20231bb8ae1b2f58cd
Angel Alemany <angelemegeele@gmail.com>
2023-04-19 22:53:35 -0400

    Update content field in BasePostModel

--- triplealianza/core/models.py
+++ triplealianza/core/models.py
@@ -8,9 +8,9 @@ class BasePostModel(models.Model):
     slug = models.CharField(max_length=150, verbose_name="Slug URL", unique=True)
     description = models.TextField(verbose_name="Descripción")
     featured_image = models.ImageField(upload_to='files', verbose_name="Imagen destacada")
+    content = RichTextUploadingField(verbose_name="Contenido")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creación")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Última edición")
-    content = RichTextUploadingField(verbose_name="Contenido")
 
     class Meta:
         abstract = True
